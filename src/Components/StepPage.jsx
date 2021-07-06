f8109fd07d97b93d3fbc4125ee526680aaa17880
Junaid Latif <junaidlatif004@gmail.com>
2021-07-06 10:32:48 +0500

    Simple Project Setup

--- src/Components/StepPage.jsx
+++ src/Components/StepPage.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
+import VectorImg from '../imgs/vector.png'
+import './CSS/StepPage.css'
+
 const StepPage = () => {
     return (
         <>
             <div className="stepPage">
-                
+                <img src={VectorImg} alt="ERROR" className="w-full absolute" />
             </div>
         </>
     )
 }
 
-export default Step1
+export default StepPage
